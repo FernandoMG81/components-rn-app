36903929da50a086c4d11dabfa5ccf68bf203d1e
Fernando Gordillo <fermg299@outlook.com>
2025-01-07 09:50:51 -0300

    agregado themed Button

--- app/animation-101/index.tsx
+++ app/animation-101/index.tsx
@@ -1,10 +1,22 @@
-import { View, Text } from 'react-native';
+import ThemedButton from '@/presentation/shared/ThemedButton';
+import ThemedView from '@/presentation/shared/ThemedView';
 
 const Animation101Screen = () => {
   return (
-    <View>
-      <Text>Animation101Screen</Text>
-    </View>
+    <ThemedView margin>
+
+      <ThemedButton
+        className='mb-5' 
+        onPress={() => console.log('FadeIn')}>FadeIn
+      </ThemedButton>
+
+      <ThemedButton
+        className='mb-5' 
+        onPress={() => console.log('FadeOut')}>FadeOut
+      </ThemedButton>
+
+
+    </ThemedView>
   );
 };
 export default Animation101Screen;
